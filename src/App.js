0c9ab13d6e03dc3b6db461e73c4ad618b1e9b99b
kosuke-17 <tamutamu17@icloud.com>
2021-05-16 11:16:08 +0900

    APIで画像の取得

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import axios from 'axios'
 import SearchBar from './components/SearchBar'
 
 const App = () => {
-    const onSearchSubmit = (term) => {
-      console.log(term);
+    const onSearchSubmit = async (term) => {
+      try {
+        const params = {
+          key: "21636522-20ab1cbc2fa3bedf8801a84b9",
+          q: term,
+        };
+        const response = await axios.get('https://pixabay.com/api/', { params });
+        console.log(response);
+      } catch {
+
+      }
     };
   return (
   <div className="ui container" style={{ marginTop: '20px' }}>
