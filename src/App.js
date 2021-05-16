2088e564f2f0a77597871875cb270fbd1a8f96ac
kosuke-17 <tamutamu17@icloud.com>
2021-05-16 11:38:15 +0900

    APIで取得した画像の表示

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React from 'react'
-import axios from 'axios'
-import SearchBar from './components/SearchBar'
+import React, { useState } from 'react';
+import axios from 'axios';
+import SearchBar from './components/SearchBar';
+import ImageList from './components/ImageList';
 
 const App = () => {
+    const [images, setImages] = useState([]);
     const onSearchSubmit = async (term) => {
       try {
         const params = {
@@ -10,14 +12,18 @@ const App = () => {
           q: term,
         };
         const response = await axios.get('https://pixabay.com/api/', { params });
-        console.log(response);
+        setImages(response.data.hits);
+        if(response.data.total===0){
+          window.alert('お探しの画像はありません。')
+        }
       } catch {
-
+        window.alert('写真の取得に失敗しました。')
       }
     };
   return (
   <div className="ui container" style={{ marginTop: '20px' }}>
     <SearchBar onSubmit={onSearchSubmit} />
+    <ImageList images={images} />
   </div>
   );
 };
